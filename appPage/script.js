ec29af98396064a01ebf59b5e8bdbda2bb6ad0d7
anxoxurado <angel975jr@gmail.com>
2024-07-15 13:40:19 -0600

    Adicion del resto de categorias, arreglo de errores en javascript

--- appPage/script.js
+++ appPage/script.js
@@ -1,68 +1,70 @@
 document.addEventListener('DOMContentLoaded', (event) => {
-    const prevButton = document.getElementById('prev-slide');
-    const nextButton = document.getElementById('next-slide');
-    const slider = document.querySelector('.container-slider');
-    const scrollbarThumb = document.querySelector('.scrollbar-thumb');
-    const scrollbarTrack = document.querySelector('.scrollbar-track');
+    const feedSections = document.querySelectorAll('.feed-section');
 
-    const scrollAmount = document.querySelector('.card-item').offsetWidth * 2;
+    feedSections.forEach(feedSection => {
+        const prevButton = feedSection.querySelector('.slide-button:first-child');
+        const nextButton = feedSection.querySelector('.slide-button:last-child');
+        const slider = feedSection.querySelector('.container-slider');
+        const scrollbarThumb = feedSection.nextElementSibling.querySelector('.scrollbar-thumb');
+        const scrollbarTrack = feedSection.nextElementSibling.querySelector('.scrollbar-track');
+        const scrollAmount = slider.querySelector('.card-item').offsetWidth * 2;
 
-    function updateButtonVisibility() {
-        const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
-        prevButton.style.display = slider.scrollLeft > 0 ? 'block' : 'none';
-        nextButton.style.display = slider.scrollLeft < maxScrollLeft ? 'block' : 'none';
-    }
-
-    function updateScrollbarThumb() {
-        const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
-        const scrollPercentage = slider.scrollLeft / maxScrollLeft;
-        const thumbWidth = scrollbarTrack.clientWidth * (slider.clientWidth / slider.scrollWidth);
-        scrollbarThumb.style.width = `${thumbWidth}px`;
-        scrollbarThumb.style.left = `${scrollPercentage * (scrollbarTrack.clientWidth - thumbWidth)}px`;
-    }
-
-    function handleThumbDrag(event) {
-        const thumbPositionRatio = (event.clientX - scrollbarTrack.getBoundingClientRect().left) / scrollbarTrack.clientWidth;
-        slider.scrollLeft = thumbPositionRatio * (slider.scrollWidth - slider.clientWidth);
-    }
-
-    let isDragging = false;
-
-    scrollbarThumb.addEventListener('mousedown', (event) => {
-        isDragging = true;
-        document.body.style.userSelect = 'none';
-    });
-
-    document.addEventListener('mousemove', (event) => {
-        if (isDragging) {
-            handleThumbDrag(event);
+        function updateButtonVisibility() {
+            const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+            prevButton.style.display = slider.scrollLeft > 0 ? 'block' : 'none';
+            nextButton.style.display = slider.scrollLeft < maxScrollLeft ? 'block' : 'none';
         }
-    });
 
-    document.addEventListener('mouseup', () => {
-        isDragging = false;
-        document.body.style.userSelect = 'auto';
-    });
+        function updateScrollbarThumb() {
+            const maxScrollLeft = slider.scrollWidth - slider.clientWidth;
+            const scrollPercentage = slider.scrollLeft / maxScrollLeft;
+            const thumbWidth = scrollbarTrack.clientWidth * (slider.clientWidth / slider.scrollWidth);
+            scrollbarThumb.style.width = `${thumbWidth}px`;
+            scrollbarThumb.style.left = `${scrollPercentage * (scrollbarTrack.clientWidth - thumbWidth)}px`;
+        }
 
-    prevButton.addEventListener('click', () => {
-        slider.scrollBy({
-            left: -scrollAmount,
-            behavior: 'smooth'
+        function handleThumbDrag(event) {
+            const thumbPositionRatio = (event.clientX - scrollbarTrack.getBoundingClientRect().left) / scrollbarTrack.clientWidth;
+            slider.scrollLeft = thumbPositionRatio * (slider.scrollWidth - slider.clientWidth);
+        }
+
+        let isDragging = false;
+        scrollbarThumb.addEventListener('mousedown', (event) => {
+            isDragging = true;
+            document.body.style.userSelect = 'none';
         });
-    });
 
-    nextButton.addEventListener('click', () => {
-        slider.scrollBy({
-            left: scrollAmount,
-            behavior: 'smooth'
+        document.addEventListener('mousemove', (event) => {
+            if (isDragging) {
+                handleThumbDrag(event);
+            }
         });
-    });
 
-    slider.addEventListener('scroll', () => {
+        document.addEventListener('mouseup', () => {
+            isDragging = false;
+            document.body.style.userSelect = 'auto';
+        });
+
+        prevButton.addEventListener('click', () => {
+            slider.scrollBy({
+                left: -scrollAmount,
+                behavior: 'smooth'
+            });
+        });
+
+        nextButton.addEventListener('click', () => {
+            slider.scrollBy({
+                left: scrollAmount,
+                behavior: 'smooth'
+            });
+        });
+
+        slider.addEventListener('scroll', () => {
+            updateButtonVisibility();
+            updateScrollbarThumb();
+        });
+
         updateButtonVisibility();
         updateScrollbarThumb();
     });
-
-    updateButtonVisibility();
-    updateScrollbarThumb();
 });
