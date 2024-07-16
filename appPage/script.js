91717905d80453fbe1ab57cc8e1fcf9e4885da4f
TheCodeOfGod <raulcano848@gmail.com>
2024-07-16 10:54:11 -0600

    Se hizo conexion a la base de datos y se muestra en al appPage

--- appPage/script.js
+++ appPage/script.js
@@ -6,9 +6,47 @@ const initSlider = () => {
         cardList.scrollBy({ left: -cardList.clientWidth, behavior: "smooth" });
     });
     nextButton.addEventListener("click", () => {
-    cardList.scrollBy({ left: cardList.clientWidth, behavior: "smooth" });
+        cardList.scrollBy({ left: cardList.clientWidth, behavior: "smooth" });
     });
 }
 
 window.addEventListener("load", initSlider);
-   
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch('http://localhost:3000/lugares/TopCafes')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error en la solicitud');
+            }
+            return response.json();
+        })
+
+        .then(data => {
+
+            const container = document.getElementById('container-cafeterias');
+            data.forEach(lugar => {
+                const divIndividual = document.createElement('div');
+                divIndividual.classList.add('card-item');
+                
+
+                divIndividual.innerHTML = `
+                    <div class="gradient-card g-orange"></div>
+                    <img src="${lugar.ruta_imgPrincipal}/${lugar.nombre_imgPrincipal}" class="local-image" alt="">
+                    <div class="card-info">
+                    <h2 class="local-name">${lugar.nombre_lugar}</h2>
+                    <p class="local-description"><span id="cat1">${lugar.caracteristica_1}</span> - <span id="cat2">${lugar.caracteristica_2}</span> - <span id="cat3">${lugar.caracteristica_3}</span> </p>
+                    </div>
+                `;
+
+                divIndividual.addEventListener('click', () => {
+                    window.location.href = `http://localhost:3001/lugares?nombre=${lugar.nombre_lugar}`;
+                });
+                container.appendChild(divIndividual);
+            });
+        })
+
+        .catch(error => {
+            console.error('Error fetching data: ', error);
+        })
+});
---
+++ conexion/server.js
@@ -0,0 +1,154 @@
+import express from 'express';
+import mysql from 'mysql';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+    });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname, '..')));
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'VACUUDB'
+});
+
+
+db.connect((err) => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the MYSQL database');
+});
+
+// ruta por default
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
+
+// SE USA PARA MOSTRAR MEJORES 10 CAFES
+app.get('/lugares/TopCafes',  (req, res) => {
+    
+    const SQLLugares = `
+    SELECT l.*, c.nombre_categoria, i.nombre_imgPrincipal, i.ruta_imgPrincipal,  a.ambiente, (select ambiente from ambientes where id_ambiente  = l.fk_ambiente2) as ambiente2
+    FROM lugares l 
+    JOIN categorias c ON l.fk_categoria = c.id_categoria
+    JOIN ambientes a ON l.fk_ambiente = id_ambiente
+    JOIN imagen_principal i ON i.fk_lugar = l.id_lugar
+    WHERE c.nombre_categoria = 'cafe' 
+    order by clicks DESC LIMIT 10 ;
+    `;
+
+    db.query(SQLLugares, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'error en la consulta de la base de datos' });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'No se encontró ningún lugar con ese nombre' });
+        }
+        res.json(results);
+    });
+
+});
+
+// SE USA PARA MOSTRAR MEJORES 10 RESTAURANTES
+app.get('/lugares/TopRestaurantes',  (req, res) => {
+    
+    const SQLLugares = `
+    SELECT l.*, c.nombre_categoria, i.nombre_imgPrincipal, i.ruta_imgPrincipal,  a.ambiente, (select ambiente from ambientes where id_ambiente  = l.fk_ambiente2) as ambiente2
+    FROM lugares l 
+    JOIN categorias c ON l.fk_categoria = c.id_categoria
+    JOIN ambientes a ON l.fk_ambiente = id_ambiente
+    JOIN imagen_principal i ON i.fk_lugar = l.id_lugar
+    WHERE c.nombre_categoria = 'restaurante' OR c.nombre_categoria = 'restaurante-bar' 
+    order by clicks DESC LIMIT 10 ;
+    `;
+
+    db.query(SQLLugares, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'error en la consulta de la base de datos' });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'No se encontró ningún lugar con ese nombre' });
+        }
+        res.json(results);
+    });
+
+});
+
+
+// SE USA PARA MOSTRAR MEJORES 10  BARES
+app.get('/lugares/TopBares',  (req, res) => {
+    
+    const SQLLugares = `
+    SELECT l.*, c.nombre_categoria, i.nombre_imgPrincipal, i.ruta_imgPrincipal,  a.ambiente, (select ambiente from ambientes where id_ambiente  = l.fk_ambiente2) as ambiente2
+    FROM lugares l 
+    JOIN categorias c ON l.fk_categoria = c.id_categoria
+    JOIN ambientes a ON l.fk_ambiente = id_ambiente
+    JOIN imagen_principal i ON i.fk_lugar = l.id_lugar
+    WHERE c.nombre_categoria = 'bar' OR c.nombre_categoria = 'restaurante-bar' 
+    order by clicks DESC LIMIT 10 ;
+    `;
+
+    db.query(SQLLugares, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'error en la consulta de la base de datos' });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'No se encontró ningún lugar con ese nombre' });
+        }
+        res.json(results);
+    });
+});
+
+
+// SE USA PARA MOSTRAR MEJORES 10 LUGARES CULTURALES
+app.get('/lugares/TopCulturales',  (req, res) => {
+    
+    const SQLLugares = `
+    SELECT l.*, c.nombre_categoria, i.nombre_imgPrincipal, i.ruta_imgPrincipal,  a.ambiente, (select ambiente from ambientes where id_ambiente  = l.fk_ambiente2) as ambiente2
+    FROM lugares l 
+    JOIN categorias c ON l.fk_categoria = c.id_categoria
+    JOIN ambientes a ON l.fk_ambiente = id_ambiente
+    JOIN imagen_principal i ON i.fk_lugar = l.id_lugar
+    WHERE c.nombre_categoria = 'culturales' order by clicks 
+    DESC LIMIT 10; ;
+    `;
+
+    db.query(SQLLugares, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'error en la consulta de la base de datos' });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ error: 'No se encontró ningún lugar con ese nombre' });
+        }
+        res.json(results);
+    });
+});
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
