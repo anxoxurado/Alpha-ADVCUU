42b0e419ff8d83389e7c3a222c280247030f429b
Elmuuquienmas <luisarma45@gmail.com>
2024-07-19 14:33:06 -0600

    l

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var popupContainer = document.getElementById('popupContainer');
   var popupFrame = document.getElementById('popupFrame');
 
-  // Aseguramos que el popup esté oculto inicialmente
   popupContainer.style.display = 'none';
 
   openBtn.onclick = function(e) {
@@ -12,20 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
     popupContainer.style.display = 'block';
   }
 
-  // Función para cerrar la ventana emergente
   function closePopup() {
     popupContainer.style.display = 'none';
     popupFrame.src = '';
   }
 
-  // Evento para cerrar la ventana emergente con la tecla Esc
   document.addEventListener('keydown', function(event) {
     if (event.key === 'Escape') {
       closePopup();
     }
   });
 
-  // Opcional: mantener la funcionalidad de cerrar al hacer clic fuera de la ventana
   window.onclick = function(event) {
     if (event.target == popupContainer) {
       closePopup();
